60498b9c6f76c5910081f539e4462fe9b5e0bfa7
Thomas Deal <justarandomgeek@gmail.com>
2019-12-17 14:49:27 -0500

    rename and cleanup

--- mod/makerelease.sh
+++ mod/makerelease.sh
@@ -7,7 +7,7 @@ modname=`cat info.json|jq -r .name`
 modver=`cat info.json|jq -r .version`
 
 # Create git tag for this version
-git tag "mod $modver"
+git tag "mod_$modver"
 
 # Prepare zip for Factorio native use and mod portal
-git archive --prefix "${modname}_$modver/" -o "${modname}_$modver.zip" HEAD
+git archive --prefix "${modname}_$modver/" -o "${modname}_$modver.zip" "mod_$modver"
