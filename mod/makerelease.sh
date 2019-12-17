7d7d52e9ed496d681a2c35981e789865070cdffb
Thomas Deal <justarandomgeek@gmail.com>
2019-12-17 12:45:42 -0500

    change tag

--- mod/makerelease.sh
+++ mod/makerelease.sh
@@ -7,7 +7,7 @@ modname=`cat info.json|jq -r .name`
 modver=`cat info.json|jq -r .version`
 
 # Create git tag for this version
-git tag "$modver"
+git tag "mod $modver"
 
 # Prepare zip for Factorio native use and mod portal
 git archive --prefix "${modname}_$modver/" -o "${modname}_$modver.zip" HEAD
