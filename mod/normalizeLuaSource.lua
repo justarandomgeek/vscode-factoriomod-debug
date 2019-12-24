bf06667d908d2758772b48209a474aa90d627e7a
Thomas Deal <justarandomgeek@gmail.com>
2019-12-24 02:51:35 -0500

    cache results of normalizeLuaSource

--- mod/normalizeLuaSource.lua
+++ mod/normalizeLuaSource.lua
@@ -14,12 +14,16 @@ if script.mod_name == "level" then
   __DebugAdapter.stepIgnore(__DebugAdapter.levelPath)
 end
 
+local knownSources = {}
+
 ---@param source string
 ---@return string
 local function normalizeLuaSource(source)
   local first = source:sub(1,1)
   if first == "=" then return source end
   if first ~= "@" then return "=(dostring)" end
+  local known = knownSources[source]
+  if known then return known end
   local modname,filename = source:match("__(.+)__/(.+)")
   if not modname then
     --startup tracing sometimes gives absolute path of the scenario script, turn it back into the usual form...
@@ -40,17 +44,24 @@ local function normalizeLuaSource(source)
 
   if modname == "level" then
     -- we *still* can't identify level properly, so just give up...
-    return string.format("LEVEL/%s",filename)
+    local result = string.format("LEVEL/%s",filename)
+    knownSources[source] = result
+    return result
   elseif modname == "core" or modname == "base" then
     -- these are under data path with no version in dir name
-    return string.format("DATA/%s/%s",modname,filename)
+    local result = string.format("DATA/%s/%s",modname,filename)
+    knownSources[source] = result
+    return result
   elseif modname == nil then
     --something totally unrecognized?
+    knownSources[source] = source
     return source
   else
     -- we found it! This will be a path relative to the `mods` directory.
     local modver = game.active_mods[modname] --TODO: script.active_mods in 0.18, allow stepping before `game`
-    return string.format("MOD/%s_%s/%s",modname,modver,filename)
+    local result = string.format("MOD/%s_%s/%s",modname,modver,filename)
+    knownSources[source] = result
+    return result
   end
 end
 __DebugAdapter.stepIgnore(normalizeLuaSource)
