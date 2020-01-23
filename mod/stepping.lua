2e90c9c1c084071874c52ebe30a9d5e7f1befdfb
Thomas Deal <justarandomgeek@gmail.com>
2020-01-22 19:50:31 -0500

    simplify on_error

--- mod/stepping.lua
+++ mod/stepping.lua
@@ -191,14 +191,10 @@ function __DebugAdapter.attach()
       end
     end
   end,"clr")
-  pcall(function()
-    --TODO: unwrap if added, remove xpcall wrapper from everything, fix stack trace offsets...
-    if script then
-      script.on_error(__DebugAdapter.on_exception)
-    elseif on_error then
-      on_error(__DebugAdapter.on_exception)
-    end
-  end)
+  -- on_error is proposed api for instrument mods to catch errors
+  if on_error then
+    on_error(__DebugAdapter.on_exception)
+  end
 end
 
 ---@param source string
