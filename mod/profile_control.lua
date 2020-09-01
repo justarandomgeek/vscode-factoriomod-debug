71621900af88d1a84cf4ccdaf83cf05e6f159c57
Thomas Deal <justarandomgeek@gmail.com>
2020-09-01 12:44:22 -0400

    .

--- mod/profile_control.lua
+++ mod/profile_control.lua
@@ -1,5 +1,5 @@
 local function begin()
-  -- start profiling early...
+  -- disable autosave so it won't try to save timers, this is a desync but i don't care.
   script.on_nth_tick(2,function(e)
     script.on_nth_tick(2,nil)
     game.autosave_enabled = false
