b009ac3e64c7ee120d1af5229bc4061057928eb1
Thomas Deal <justarandomgeek@gmail.com>
2020-09-21 23:18:53 -0400

    remove old log hook

--- mod/log.lua
+++ mod/log.lua
@@ -41,43 +41,10 @@ if __DebugAdapter.instrument then
   if not base_version then
     log = newlog
     return
-  end  
+  end
   local a,b,c = base_version:match("(%d+).(%d+).(%d+)")
   if (a=="1" or (a=="0" and b=="18" and (tonumber(c) or 0)>=34)) then
     log = newlog
     return
   end
 end
-
--- older version still require the evil version of the hook
-newlog("installing legacy `log` hook in " .. (data and "data" or script.mod_name or "?"))
-
-log = nil
-local gmeta = getmetatable(_ENV)
-if not gmeta then
-  gmeta = {}
-  setmetatable(_ENV,gmeta)
-end
-local oldindex = gmeta.__index
-local do_old_index = ({
-  ["nil"] = function(t,k)
-    return nil
-  end,
-  ["function"] = oldindex,
-  ["table"] = function(t,k)
-    return oldindex[k]
-  end,
-})[type(oldindex)]
-
-function gmeta.__index(t,k)
-  if k == "log" then
-    local parent = debug.getinfo(2,"n")
-    if parent then
-      return newlog
-    else
-      return oldlog
-    end
-  end
-  return do_old_index(t,k)
-end
-__DebugAdapter.stepIgnore(gmeta.__index)
