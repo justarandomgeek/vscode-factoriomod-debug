25b09341ac76aa5f0030ab6002c0d40a1a0989da
Thomas Deal <justarandomgeek@gmail.com>
2021-01-19 01:28:23 -0500

    stale comments

--- mod/instrument-settings.lua
+++ mod/instrument-settings.lua
@@ -13,9 +13,6 @@ debug.getregistry().__DASentModules = true
 print("DBG: on_instrument_settings")
 debug.debug()
 if __DebugAdapter then
-  -- on_error is a global available in "Instrument Mode"
-  -- This controls the insertion of debug hooks (don't need xpcall for break-on-exception) and
-  -- stack frame hiding (don't need to hide xpcall)
   __DebugAdapter.instrument = true
   require("__debugadapter__/debugadapter.lua")
 end
