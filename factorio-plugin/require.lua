e2026e0b6921ab1aaf27ea13193b1395d3b21b1d
Nicholas Dunn <nexela@gmail.com>
2022-07-22 15:21:33 -0400

    Revert "Drop __modname__ all together"
    This reverts commit 71643b11d68148cb3e74808e3a128d166576fda3.

--- factorio-plugin/require.lua
+++ factorio-plugin/require.lua
@@ -13,8 +13,7 @@ local function replace(_, text, diffs)
     local original_name = name
 
     ---Convert the mod name prefix if there is one
-    -- name = name:gsub("^__(.-)__", "%1")
-    name = name:gsub("^__(.-)__[%./]", "")
+    name = name:gsub("^__(.-)__", "%1")
 
     if name ~= original_name then
       util.add_diff(diffs, start, finish, name)
