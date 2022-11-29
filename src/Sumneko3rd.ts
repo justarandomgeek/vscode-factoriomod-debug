ba3e5b5f46d758ab6bcc01b4fb04f0e21df78220
justarandomgeek <justarandomgeek@gmail.com>
2022-11-29 11:41:16 -0500

    config.lua -> config.json

--- src/Sumneko3rd.ts
+++ src/Sumneko3rd.ts
@@ -2,11 +2,18 @@
 const sumneko3rdFiles: { name: string; content: string }[] = [];
 // @ts-ignore
 import { default as files, filenames } from './sumneko-3rd/**/*.lua';
+import config from "./sumneko-3rd/factorio/config.json";
+
 for (let i = 0; i < files.length; i++) {
 	sumneko3rdFiles.push({
 		name: (filenames[i] as string).replace("./sumneko-3rd/", ""),
 		content: files[i].default,
 	});
 }
+
+sumneko3rdFiles.push({
+	name: "factorio/config.json",
+	content: JSON.stringify(config),
+});
 
 export default sumneko3rdFiles;
