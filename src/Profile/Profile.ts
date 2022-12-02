f9557c4c092adec26a16d52547cefcf229e62127
justarandomgeek <justarandomgeek@gmail.com>
2022-12-02 00:26:35 -0500

    imports

--- src/Profile/Profile.ts
+++ src/Profile/Profile.ts
@@ -1,9 +1,6 @@
 import * as vscode from "vscode";
 import { assert } from "console";
 
-//@ts-ignore
-import FlamegraphHtml from "./Flamegraph.html";
-
 function NaN_safe_max(a:number, b:number):number {
 	if (isNaN(a)) { return b; }
 	if (isNaN(b)) { return a; }
@@ -307,9 +304,11 @@ export class Profile implements vscode.Disposable  {
 			}
 		);
 		const flameview = this.flamePanel.webview;
-		flameview.html = (<string>FlamegraphHtml)
-			.replace("$Flamegraph.css$", flameview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "/dist/Flamegraph.css")).toString())
-			.replace("$Flamegraph.js$", flameview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "/dist/Flamegraph.js")).toString());
+		flameview.html =
+			//@ts-expect-error
+			(<string>(await import("./Flamegraph.html")))
+				.replace("$Flamegraph.css$", flameview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "/dist/Flamegraph.css")).toString())
+				.replace("$Flamegraph.js$", flameview.asWebviewUri(vscode.Uri.joinPath(this.context.extensionUri, "/dist/Flamegraph.js")).toString());
 
 		flameview.onDidReceiveMessage(
 			(mesg:{command:"init"}|{command:"click";name:string;filename?:string;line?:number})=>{
