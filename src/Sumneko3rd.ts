8ea0fdcb17074ec44d9d6de5b5d1ac600eddcf59
justarandomgeek <justarandomgeek@gmail.com>
2022-12-03 15:58:53 -0500

    write factorio version in sumneko-3rd config.json

--- src/Sumneko3rd.ts
+++ src/Sumneko3rd.ts
@@ -1,33 +1,38 @@
 import {version}  from "../package.json";
-let sumneko3rdFiles: { name: string; content: string }[];
+let sumneko3rdLuaFiles: { name: string; content: string }[];
 
 
 // zip together the two arrays of imports...
-export default async function() {
-	if (!sumneko3rdFiles) {
 
-		sumneko3rdFiles = [];
+
+export async function getLuaFiles() {
+	if (!sumneko3rdLuaFiles) {
+
+		sumneko3rdLuaFiles = [];
 		// @ts-ignore
 		const glob = await import('../sumneko-3rd/**/*.lua');
 		const files = glob.default;
 		const filenames = glob.filenames;
 
 		for (let i = 0; i < files.length; i++) {
-			sumneko3rdFiles.push({
+			sumneko3rdLuaFiles.push({
 				name: (filenames[i] as string).replace("../sumneko-3rd/", ""),
 				content: files[i].default,
 			});
 		}
-
-		sumneko3rdFiles.push({
-			name: "factorio/config.json",
-			content: JSON.stringify(Object.assign(
-				await import("../sumneko-3rd/factorio/config.json"),
-				{
-					bundleVersion: version,
-				})),
-		});
 	}
 
-	return sumneko3rdFiles;
+	return sumneko3rdLuaFiles;
+}
+
+export async function getConfig(factorioVersion?:string) {
+	return {
+		name: "factorio/config.json",
+		content: JSON.stringify(Object.assign(
+			await import("../sumneko-3rd/factorio/config.json"),
+			{
+				bundleVersion: version,
+				factorioVersion: factorioVersion,
+			})),
+	};
 }
--- src/cli/docs.ts
+++ src/cli/docs.ts
@@ -7,19 +7,25 @@ program.command("sumneko-3rd [outdir]")
 	.description("Generate a library bundle for sumneko.lua LSP")
 	.option("-d, --docs <docsjson>", "Include runtime docs")
 	.action(async (outdir:string|undefined, options:{docs?:string})=>{
-		await Promise.all((await (await import("../Sumneko3rd")).default()).map(async (file)=>{
+		const sumneko3rd = await import("../Sumneko3rd");
+		await Promise.all((await sumneko3rd.getLuaFiles()).map(async (file)=>{
 			const filepath = path.join(outdir ?? process.cwd(), file.name);
 			await fsp.mkdir(path.dirname(filepath), { recursive: true });
 			return fsp.writeFile(filepath, Buffer.from(file.content));
 		}));
+		let factorionVersion:string|undefined;
 		if (options.docs) {
-			await docscommand(options.docs, path.join(outdir ?? process.cwd(), "factorio", "library"));
+			factorionVersion = await docscommand(options.docs, path.join(outdir ?? process.cwd(), "factorio", "library"));
 		}
+		const config = await sumneko3rd.getConfig(factorionVersion);
+		await fsp.writeFile(path.join(outdir ?? process.cwd(), config.name), Buffer.from(config.content));
 	});
 
 program.command("docs <docjson> <outdir>")
 	.description("Generate runtime api docs for sumneko.lua LSP")
-	.action(docscommand);
+	.action(async (docjson:string, outdir:string)=>{
+		await docscommand(docjson, outdir);
+	});
 async function docscommand(docjson:string, outdir:string) {
 	const { ApiDocGenerator } = await import('../ApiDocs/ApiDocGenerator');
 	const docs = new ApiDocGenerator((await fsp.readFile(docjson, "utf8")).toString(), await getConfigGetter("doc", {}));
@@ -27,4 +33,5 @@ async function docscommand(docjson:string, outdir:string) {
 	await docs.generate_sumneko_docs(async (filename:string, buff:Buffer)=>{
 		await fsp.writeFile(path.join(outdir, filename), buff);
 	});
+	return docs.application_version;
 };
