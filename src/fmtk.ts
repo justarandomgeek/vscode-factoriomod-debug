26bac2583b0197391e8f9f621b40e3ba606f1dcc
justarandomgeek <justarandomgeek@gmail.com>
2022-12-05 16:15:14 -0500

    .

--- src/fmtk.ts
+++ src/fmtk.ts
@@ -1,9 +1,13 @@
 #!/usr/bin/env node
+
+// When loaded by vscode, `activate` loads the extension code
 import type { ExtensionContext } from 'vscode';
 export async function activate(context:ExtensionContext) {
 	const extension = await import("./vscode/extension");
 	extension.activate(context);
 }
+
+// When run from the command line, import `main` for CLI interface
 if (require.main === module) {
 	import("./cli/main");
 }
