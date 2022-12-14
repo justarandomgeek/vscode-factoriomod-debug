caa4fa72c29553a5bd5058775cf6c1b87b7ac68f
JanSharp <steinmeister36@gmail.com>
2022-12-14 17:59:36 +0100

    Simplify and optimize the `command-line` module
    Add support for `/measured-command`

--- factorio-plugin/command-line.lua
+++ factorio-plugin/command-line.lua
@@ -2,44 +2,24 @@
 
 local util = require("factorio-plugin.util")
 
+local commands_lut = {
+  ["command"] = true,
+  ["c"] = true,
+  ["silent-command"] = true,
+  ["sc"] = true,
+  ["measured-command"] = true,
+}
+
 ---@param uri string @ The uri of file
 ---@param text string @ The content of file
 ---@param diffs Diff[] @ The diffs to add more diffs to
 local function replace(uri, text, diffs)
-  -- Remove the `/sc ` completely and replace with blank space.
-  ---@type string|number
-  for s_remove, s_id
-  in
-    text:gmatch("()/silent-command ()")
+  for s_command, command, f_command in
+    util.gmatch_at_start_of_line(text, "()/([a-z-]+%f[%s\0])()")--[[@as fun():integer, string, integer]]
   do
-    util.add_diff(diffs, s_remove, s_id, string.rep(" ", s_id - s_remove))
-  end
-  
-  -- Remove the `/sc ` completely and replace with blank space.
-  ---@type string|number
-  for s_remove, s_id
-  in
-    text:gmatch("()/sc ()")
-  do
-    util.add_diff(diffs, s_remove, s_id, string.rep(" ", s_id - s_remove))
-  end
-  
-  -- Remove the `/command ` completely and replace with blank space.
-  ---@type string|number
-  for s_remove, s_id
-  in
-    text:gmatch("()/command ()")
-  do
-    util.add_diff(diffs, s_remove, s_id, string.rep(" ", s_id - s_remove))
-  end
-  
-  -- Remove the `/c ` completely and replace with blank space.
-  ---@type string|number
-  for s_remove, s_id
-  in
-    text:gmatch("()/c ()")
-  do
-    util.add_diff(diffs, s_remove, s_id, string.rep(" ", s_id - s_remove))
+    if commands_lut[command] then
+      util.add_diff(diffs, s_command, f_command, "")
+    end
   end
 end
 
