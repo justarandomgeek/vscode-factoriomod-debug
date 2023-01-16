53da159dd8c4b6d156a6651cc20348550b803778
justarandomgeek <justarandomgeek@gmail.com>
2023-01-16 11:24:49 -0500

    actually run a test

--- test/debug.ts
+++ test/debug.ts
@@ -1,13 +1,38 @@
+import * as inspector from "inspector";
+import { setup, teardown, test } from "mocha";
 import { DebugClient } from "@vscode/debugadapter-testsupport";
+import * as testversion from "./testversion.json";
 
 
 let dc: DebugClient;
 
 setup( ()=>{
-	dc = new DebugClient('node', '../dist/fmtk.js', 'factoriomod', {
-
+	dc = new DebugClient('node', './dist/fmtk.js', 'factoriomod', {
+		env: Object.assign({},
+			process.env,
+			{
+				FMTK_TEST_INSPECT: inspector.url() ? "34198" : undefined,
+				FMTK_TEST_ARGV: JSON.stringify([
+					"debug", testversion.factorioPath,
+					"-d", testversion.docsPath,
+				]),
+			},
+		),
 	});
+	dc.defaultTimeout=60000;
 	return dc.start();
 });
 
 teardown( ()=>dc.stop() );
+
+test('should launch', async ()=>{
+	return Promise.all([
+		dc.configurationSequence(),
+		dc.launch({
+			"type": "factoriomod",
+			"request": "launch",
+			"factorioArgs": ["--load-scenario", "base/freeplay"],
+		}),
+		dc.waitForEvent('terminated'),
+	]);
+});
