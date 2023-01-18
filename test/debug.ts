abd69c98e5ab872385aaa2c344c92c8c9a295e11
justarandomgeek <justarandomgeek@gmail.com>
2023-01-18 11:40:42 -0500

    run tests from local zip install

--- test/debug.ts
+++ test/debug.ts
@@ -1,38 +1,57 @@
 import * as inspector from "inspector";
+import * as path from "path";
 import { setup, teardown, test } from "mocha";
 import { DebugClient } from "@vscode/debugadapter-testsupport";
-import * as testversion from "./testversion.json";
-
+import type { LaunchRequestArguments } from "../src/Debug/factorioModDebug";
+import type { OutputEvent } from "@vscode/debugadapter";
 
 let dc: DebugClient;
 
-setup( ()=>{
-	dc = new DebugClient('node', './dist/fmtk.js', 'factoriomod', {
+setup(async ()=>{
+	dc = new DebugClient('node', path.join(process.cwd(), './dist/fmtk.js'), 'factoriomod', {
+		cwd: path.join(process.cwd(), "./test/factorio/mods"),
 		env: Object.assign({},
 			process.env,
 			{
 				FMTK_TEST_INSPECT: inspector.url() ? "34198" : undefined,
 				FMTK_TEST_ARGV: JSON.stringify([
-					"debug", testversion.factorioPath,
-					"-d", testversion.docsPath,
+					"debug",
+					path.join(process.cwd(), "./test/factorio/bin/x64/factorio.exe"),
 				]),
 			},
 		),
+		// for some reason not being detached makes factorio's stdin break (when it reopens it?)
+		detached: true,
 	});
-	dc.defaultTimeout=60000;
-	return dc.start();
+	dc.on('output', (e:OutputEvent)=>{
+		console.log(`${e.body.category} ${e.body.output}`);
+	});
+	await dc.start();
+	// long timeouts because we're loading factorio...
+	dc.defaultTimeout = 30000;
+	// or fake "socket" to disable timeouts
+	//(dc as any)._socket = { end: ()=>{} };
 });
 
-teardown( ()=>dc.stop() );
+teardown(async ()=>{
+	await dc.stop();
+});
 
 test('should launch', async ()=>{
-	return Promise.all([
-		dc.configurationSequence(),
-		dc.launch({
-			"type": "factoriomod",
-			"request": "launch",
-			"factorioArgs": ["--load-scenario", "base/freeplay"],
-		}),
-		dc.waitForEvent('terminated'),
-	]);
+	await dc.launch({
+		type: "factoriomod",
+		request: "launch",
+		factorioArgs: ["--load-scenario", "debugadapter-tests/terminate"],
+		//TODO: have a test before this one ensure these are installed?
+		// free chance for bundle+portal installs, but portal needs download token
+		adjustMods: {
+			"debugadapter-tests": true,
+			"minimal-no-base-mod": true,
+		},
+		disableExtraMods: true,
+		allowDisableBaseMod: true,
+		//trace: true,
+	} as LaunchRequestArguments);
+	await dc.configurationSequence();
+	await dc.waitForEvent('terminated');
 });
