6af3a705c971a9462d4a4f9bd9599995c4844bf6
justarandomgeek <justarandomgeek@gmail.com>
2023-05-01 00:28:50 -0400

    clobber extensions on slashed names and tweak search path to better match factorio require

--- sumneko-3rd/factorio/factorio-plugin/require.lua
+++ sumneko-3rd/factorio/factorio-plugin/require.lua
@@ -15,6 +15,14 @@ local function replace(_, text, diffs)
     ---Convert the mod name prefix if there is one
     name = name:gsub("^__(.-)__", "%1")
 
+    ---If the path has slashes in it, it may also have an extension
+    ---the LS is not expecting. Factorio would also clobber any extension
+    ---to .lua anyway. This just strips it to go with the default `?.lua`
+    ---search pattern in "Lua.runtime.path"
+    if name:match("[\\/]") then
+      name = name:gsub("%.%a+$", "")
+    end
+
     if name ~= original_name then
       util.add_diff(diffs, start, finish, name)
     end
