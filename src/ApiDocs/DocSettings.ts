1b3ec0d2eae52790b9a47a3c3bdde70587189049
justarandomgeek <justarandomgeek@gmail.com>
2023-09-19 17:07:46 -0400

    cleanup

--- src/ApiDocs/DocSettings.ts
+++ src/ApiDocs/DocSettings.ts
@@ -1,13 +1,3 @@
-export type ApiBuiltinCustom =
-	{kind:"none"} |
-	{kind:"alias"; base:string} |
-	{kind:"class"; base:string[]; operators?:boolean};
-
 export interface DocSettings {
 	docLinksVersion?:"latest"|"current"
-	signedUMinus?:boolean
-	builtinOperators?:boolean
-	builtinCustomStyle?:{[k:string]:ApiBuiltinCustom}
-	useInteger?:boolean
-	numberStyle?:"alias"|"class"|"aliasNative"
 }
