32b1311fba6832384037e47f36e4bcc62e572f75
justarandomgeek <justarandomgeek@gmail.com>
2023-11-28 20:59:54 -0500

    specify protos json in docs test

--- test/docs.spec.ts
+++ test/docs.spec.ts
@@ -5,10 +5,14 @@ import { forkTest } from "./util";
 suite('CLI Docs', ()=>{
 	const fmtk = path.join(__dirname, '../dist/fmtk.js');
 	const jsonpath = path.join(__dirname, "factorio/doc-html/runtime-api.json");
+	const protospath = path.join(__dirname, "factorio/doc-html/prototype-api.json");
 	const cwd = path.join(__dirname, "../");
 
 	test('sumneko-3rd', async ()=>{
-		await forkTest(fmtk, ["sumneko-3rd", "-d", jsonpath, path.join(__dirname, "../out/sumneko-3rd")], {cwd: cwd});
+		await forkTest(fmtk, ["sumneko-3rd",
+			"-d", jsonpath,
+			"-p", protospath,
+			path.join(__dirname, "../out/sumneko-3rd")], {cwd: cwd});
 	});
 
 });
