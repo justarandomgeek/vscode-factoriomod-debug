02bbde3eaf4821c9fba67787040a90a6151afc52
Jan Mischak <jan.mischak@kstp.at>
2024-02-22 01:58:37 +0100

    Improve object_name gmatch pattern significantly
    Improve overall plugin performance ~2.5 times

--- luals-addon/factorio/factorio-plugin/object-name.lua
+++ luals-addon/factorio/factorio-plugin/object-name.lua
@@ -7,12 +7,17 @@ local object_name_module_flag = util.module_flags.object_name
 ---@param text string @ The content of file
 ---@param diffs Diff[] @ The diffs to add more diffs to
 local function replace(_, text, diffs)
-  -- p_front is 1 after the front
   util.reset_is_disabled_to_file_start()
-  ---@type string, integer, integer, integer
-  for preceding_text, p_front, p_dot, f_obj_name in
-    util.gmatch_at_start_of_line(text, "([^\n]-)()[a-zA-Z_][a-zA-Z0-9_]*%s*()%.%s*object_name()%f[^a-zA-Z0-9_]")
+  ---@type integer, string, integer
+  for line_start, preceding_text, f_obj_name in
+    util.gmatch_at_start_of_line(text, "()([^\n]-)object_name()%f[^a-zA-Z0-9_]")
   do
+    -- p_front is 1 after the front
+    local p_front, p_dot = string.match(preceding_text, "()[a-zA-Z_][a-zA-Z0-9_]*%s*()%.%s*$")
+    if not p_front then goto continue end
+    p_front = line_start + p_front - 1
+    p_dot = line_start + p_dot - 1
+    preceding_text = string.sub(text, line_start--[[@as integer]], p_front - 1)
     if preceding_text:find("%.%s*$")
       or preceding_text:find("function%s*$")
       or util.is_disabled(p_front, object_name_module_flag)
