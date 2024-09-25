2832452c120844c7e243f8961e7ede55a6617c95
justarandomgeek <justarandomgeek@gmail.com>
2024-09-24 20:50:35 -0400

    generate from online docs

--- src/cli/docs.ts
+++ src/cli/docs.ts
@@ -2,27 +2,55 @@ import * as fsp from 'fs/promises';
 import path from 'path';
 import { program } from 'commander';
 import { getConfig } from "./util";
+import { default as fetch } from "node-fetch";
 import { createWriteStream } from 'fs';
 import { remark } from "remark";
 import { visit } from "unist-util-visit";
 import type { VFile } from "vfile";
 import type { Root, Link } from "mdast";
 
+
 import { ApiDocGenerator, ProtoDocGenerator, LuaLSAddon } from "../fmtk";
 
+
+async function fetch_docs(url:string) {
+	const result = await fetch(url);
+	if (!result.ok) {
+		throw new Error(`Error fetching ${url} : ${result.statusText}`);
+	}
+	return result.text();
+}
 
 program.command("luals-addon [outdir]")
 	.alias("sumneko-3rd")
 	.description("Generate a library bundle for LuaLS (sumneko.lua) LSP")
-	.requiredOption("-d, --docs <docsjson>", "Runtime docs")
-	.requiredOption("-p, --protos <protosjson>", "Prototype docs")
-	.action(async (outdir:string|undefined, options:{docs:string; protos:string})=>{
+	.option("-d, --docs <docsjson>", "Runtime docs")
+	.option("-p, --protos <protosjson>", "Prototype docs")
+	.option("-o, --online [version]", "Use online docs")
+	.action(async (outdir:string|undefined, options:{docs?:string; protos?:string; online?:string})=>{
 		const docsettings = await getConfig("docs", {});
 		const libdir = path.join(outdir ?? process.cwd(), "factorio", "library");
 
-		const docs = new ApiDocGenerator((await fsp.readFile(options.docs, "utf8")).toString(), docsettings);
+		let docsjson:string;
+		let protosjson:string;
+		if (options.docs && options.protos) {
+			// use files
+			docsjson = (await fsp.readFile(options.docs, "utf8")).toString();
+			protosjson = (await fsp.readFile(options.protos, "utf8")).toString();
+		} else if (options.docs || options.protos) {
+			// error: must specify both
+			throw new Error("Using local json files must specify both files");
+		} else {
+			// use online...
+			const version = options.online ?? "latest";
 
-		const pdocs = new ProtoDocGenerator((await fsp.readFile(options.protos, "utf8")).toString(), docsettings);
+			docsjson = await fetch_docs(`https://lua-api.factorio.com/${version}/runtime-api.json`);
+			protosjson = await fetch_docs(`https://lua-api.factorio.com/${version}/prototype-api.json`);
+		}
+
+		const docs = new ApiDocGenerator(docsjson, docsettings);
+		const pdocs = new ProtoDocGenerator(protosjson, docsettings);
+
 		const resolve_link = (node:Link)=>{
 			const matches = node.url.match(/^(runtime|prototype):(.+?)(?:::(.+))?$/);
 			if (matches) {
