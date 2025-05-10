084ba47d66896616ece08e7dc2a0b205a2d1c0e6
justarandomgeek <justarandomgeek@gmail.com>
2025-05-09 22:24:51 -0400

    add item-weight to aux links

--- src/ApiDocs/ApiDocGenerator.ts
+++ src/ApiDocs/ApiDocGenerator.ts
@@ -83,7 +83,7 @@ export class ApiDocGenerator<V extends ApiVersions = ApiVersions> {
 		if (member === 'builtin_types') {
 			return `/builtin-types.html${part}`;
 		}
-		if (['libraries', 'storage', 'migrations', 'data-lifecycle', 'prototype-tree', 'noise-expressions', 'instrument'].includes(member)) {
+		if (['libraries', 'storage', 'migrations', 'data-lifecycle', 'prototype-tree', 'noise-expressions', 'instrument', 'item-weight'].includes(member)) {
 			return `/auxiliary/${member}.html${part}`;
 		}
 		if (this.concepts.has(member)) {
