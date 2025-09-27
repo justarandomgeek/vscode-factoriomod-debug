5ff23624091f4c0b581e9cd77e1267c28ff4c255
justarandomgeek <justarandomgeek@gmail.com>
2025-09-27 15:54:38 -0400

    auto-fix vscode engines version when types update

--- postinstall.ts
+++ postinstall.ts
@@ -1,8 +1,15 @@
 import { execSync } from "child_process";
+import { applyEdits, modify } from "jsonc-parser";
+import * as fsp from 'fs/promises';
 
 if (process.env.NODE_ENV !== 'production') {
 	console.log('Running dev postinstall...');
 	execSync("npx patch-package", { stdio: 'inherit' });
+
+	const json = await fsp.readFile("package.json", "utf8");
+	const vscode_types = JSON.parse(json).devDependencies["@types/vscode"] as string;
+	const edits = modify(json, ["engines", "vscode"], vscode_types, {});
+	await fsp.writeFile("package.json", applyEdits(json, edits));
 } else {
 	console.log('Skipping dev postinstall');
 }
