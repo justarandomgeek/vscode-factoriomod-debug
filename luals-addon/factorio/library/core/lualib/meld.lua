9fb283ee381f824c21a304d13c0c897fedc55088
justarandomgeek <justarandomgeek@gmail.com>
2026-07-09 23:18:06 -0400

    meld namespace

--- luals-addon/factorio/library/core/lualib/meld.lua
+++ luals-addon/factorio/library/core/lualib/meld.lua
@@ -1,57 +1,57 @@
 ---@meta "meld"
 
+---@namespace meld
+
 ---@class meld
----@field private control_marker meld.control_marker
----@field private control_handlers meld.control_handlers
----@overload fun<T>(target:T, source:meld.source<T>):T
+---@field private control_marker control_marker
+---@field private control_handlers control_handlers
+---@overload fun<T>(target:T, source:source<T>):T
 local meld = {}
 
----@class (exact) meld.control_marker
-meld.control_marker = {} -- empty but unique table used as a marker
+---@class (exact) control_marker empty but unique table used as a marker
 
----@class meld.control_handlers
-meld.control_handlers = {}
+---@class control_handlers
 
----@class (exact) meld.control_op
----@field private marker meld.control_marker
+---@class (exact) control_op
+---@field private marker control_marker
 
----@class (exact) meld.delete_op : meld.control_op
+---@class (exact) delete_op : control_op
 ---@field private op "delete"
 
----@return meld.delete_op
+---@return delete_op
 meld.delete = function() end
 
 ---@generic T
----@class (exact) meld.overwrite_op<T> : meld.control_op
+---@class (exact) overwrite_op<T> : control_op
 ---@field private op "overwrite"
 ---@field private data T
 
 ---@generic T
 ---@param new T
----@return meld.overwrite_op<T>
+---@return overwrite_op<T>
 meld.overwrite = function(new) end
 
 ---@generic T
----@class (exact) meld.invoke_op<T> : meld.control_op
+---@class (exact) invoke_op<T> : control_op
 ---@field private op "invoke"
 ---@field private fct fun(v:T):T
 
 ---@generic T
 ---@param fct fun(v:T):T
----@return meld.invoke_op<T>
+---@return invoke_op<T>
 meld.invoke = function(fct) end
 
 ---@generic T
----@class (exact) meld.append_op<T> : meld.control_op
+---@class (exact) append_op<T> : control_op
 ---@field private op "append"
 ---@field private data T
 
 ---@generic T
 ---@param T[]
----@return meld.append_op<T>
+---@return append_op<T>
 meld.append = function(data) end
 
----@alias meld.source<T> (T extends table and {[K in keyof T]?: meld.source<T[K]>} or T)|meld.delete_op|meld.overwrite_op<T>|meld.invoke_op<T>|(T extends A[] and meld.append_op<A> or never)
+---@alias source<T> (T extends table and {[K in keyof T]?: source<T[K]>} or T)|delete_op|overwrite_op<T>|invoke_op<T>|(T extends A[] and append_op<A> or never)
 
 --- recursive table merge but it reuses target table (does not deepcopy it). When target is not to be reused or more than
 ---  2 tables are to be merged, consider using util.merge. When there is conflict of 2 values, a value from the source will
@@ -59,7 +59,7 @@ meld.append = function(data) end
 ---  be possible under normal merge rules
 ---@generic T
 ---@param target T
----@param source meld.source<T>
+---@param source source<T>
 ---@return T
 meld.meld = function(target, source) end
 
