40ca437fc128d48aa57c19382804394bcba1ef67
justarandomgeek <justarandomgeek@gmail.com>
2026-07-09 23:58:49 -0400

    debugadapter meta

--- luals-addon/factorio/library/runtime/debugadapter.lua
+++ luals-addon/factorio/library/runtime/debugadapter.lua
@@ -2,3 +2,7 @@
 
 ---@type LuaDebugAdapter
 debugadapter = {} --[[@as LuaDebugAdapter]]
+
+---@class (partial) std.metatable
+---@field __debugcounts fun(self):int32,int32
+---@field __debugchildren fun(self, filters:DebugVariablesFilter): DebugVariables[]
