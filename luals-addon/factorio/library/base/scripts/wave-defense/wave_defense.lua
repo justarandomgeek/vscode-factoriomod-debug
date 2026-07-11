d14395891d86c7ad8e9537256f0d4c52775c463b
justarandomgeek <justarandomgeek@gmail.com>
2026-07-11 15:31:54 -0400

    wave_defense config

--- luals-addon/factorio/library/base/scripts/wave-defense/wave_defense.lua
+++ luals-addon/factorio/library/base/scripts/wave-defense/wave_defense.lua
@@ -5,11 +5,35 @@
 
 ---@namespace WaveDefense
 
+---@alias ItemMap table<data.ItemName, ItemCountType>
+
 ---@class (exact) Events
 ---@field on_round_started defines.events
 
+---@class DaySettings
+---@field ticks_per_day MapTick
+---@field dusk number
+---@field evening number
+---@field morning number
+---@field dawn number
+
+---@class Difficulty
+---@field starting_area_size number
+---@field day_settings DaySettings
+---@field starting_chest_items ItemMap
+---@field respawn_items ItemMap
+---@field bounties table<data.EntityName, integer>
+---@field unit_waves table<data.EntityName, [integer, integer|nil]>
+---@field wave_power_function "default"|"hard"
+---@field speed_multiplier_function "default"
+---@field starting_evolution_factor number
+---@field bounty_modifier number
+---@field unit_prices table<data.EntityName, integer>
+
 ---@class Config
---TODO
+---@field difficulties table<keyof defines.difficulty,Difficulty>
+---@field map_gen_settings MapGenSettings
+---@field infinite boolean
 
 ---@class RemoteInterface
 ---@field set_config fun(config:Config)
