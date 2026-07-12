ec959e2984bdd602e8780a0a6fc198d0c97a1390
justarandomgeek <justarandomgeek@gmail.com>
2026-07-12 19:30:19 -0400

    meld non-recursive type

--- luals-addon/factorio/library/core/lualib/meld.lua
+++ luals-addon/factorio/library/core/lualib/meld.lua
@@ -48,8 +48,16 @@ meld.invoke = function(fct) end
 ---@return append_op<T>
 meld.append = function(data) end
 
----@alias field<T> (T extends (string|number|boolean|integer|nil) and T or source<T>)|delete_op|overwrite_op<T>|invoke_op<T>|(T extends infer A[] and append_op<A> or never)
----@alias source<T> {[K in keyof T]?: field<T[K]>}
+-- emmy can't handle fully recursive mapped types, so just do a reasonable number of levels...
+
+---@alias op<T> delete_op|overwrite_op<T>|invoke_op<T>|(T extends infer A[] and append_op<A> or never)
+
+---@alias field5<T> T|op<T>|(T extends {[infer KK]: infer VV} and {[K in keyof T]?: T[K]} or never)
+---@alias field4<T> T|op<T>|(T extends {[infer KK]: infer VV} and {[K in keyof T]?: field5<T[K]>} or never)
+---@alias field3<T> T|op<T>|(T extends {[infer KK]: infer VV} and {[K in keyof T]?: field4<T[K]>} or never)
+---@alias field2<T> T|op<T>|(T extends {[infer KK]: infer VV} and {[K in keyof T]?: field3<T[K]>} or never)
+---@alias field<T> T|op<T>|(T extends {[infer KK]: infer VV} and {[K in keyof T]?: field2<T[K]>} or never)
+---@alias source<T> {[K in keyof T]?: field<T[K]>}|T
 
 --- recursive table merge but it reuses target table (does not deepcopy it). When target is not to be reused or more than
 ---  2 tables are to be merged, consider using util.merge. When there is conflict of 2 values, a value from the source will
