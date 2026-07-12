4839194025c589058ba9e41bd0f1d604cceb3f47
justarandomgeek <justarandomgeek@gmail.com>
2026-07-11 20:45:47 -0400

    better never

--- luals-addon/factorio/library/core/lualib/event_handler.lua
+++ luals-addon/factorio/library/core/lualib/event_handler.lua
@@ -5,6 +5,7 @@
 ---@field on_achievement_gained EventData.on_achievement_gained
 ---@field on_ai_command_completed EventData.on_ai_command_completed
 ---@field on_segmented_unit_died EventData.on_segmented_unit_died
+---@field on_nonsense EventData.on_nonsense extends EventData and EventData.on_nonsense or EventData
 ---@field [string] EventData
 --- like 300 more
 
@@ -21,17 +22,16 @@
 ---@type EventMapDefines
 local devents
 
-
 ---@generic const E : keyof EventDataMap
 ---@param event EventToken<E>|E|(EventToken<E>|E)[]
----@param func fun(e:EventDataMap[E])|nil
----@param filters? (EventFilterMap[E])[]
----@overload fun(event:EventToken<E>|E, func: fun(e:EventDataMap[E]), filters: (EventFilterMap[E])[])
+---@param func? fun(e:EventDataMap[E])|nil
+---@param filters E extends keyof EventFilterMap and (EventFilterMap[E])[] or never
+---@overload fun(event:EventToken<E>|E, func: fun(e:EventDataMap[E]), filters: E extends keyof EventFilterMap and (EventFilterMap[E])[] or never)
 ---@overload fun(event:EventToken<E>|E, func?: fun(e:EventDataMap[E])|nil)
 ---@overload fun(event:(EventToken<E>|E)[], func?: fun(e:EventDataMap[E])|nil)
 function on_event(event, func, filters) end
 
-on_event("on_achievement_gained", function(e) end)
+on_event("on_achievement_gained", function(e) end, {{filter=""}})
 on_event("on_something_else", function(e) end )
 on_event(devents.on_achievement_gained, function(e) end )
 on_event({"on_achievement_gained", "on_ai_command_completed"}, function(e) end )
