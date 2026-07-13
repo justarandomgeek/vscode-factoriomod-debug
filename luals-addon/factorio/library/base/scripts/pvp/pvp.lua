25b930cfcf1102b09874fbdbe74f88160c7fb74a
PennyJim <git@pennyjim.com>
2026-07-13 01:17:44 -0600

    Add more descriptions and fix some types

--- luals-addon/factorio/library/base/scripts/pvp/pvp.lua
+++ luals-addon/factorio/library/base/scripts/pvp/pvp.lua
@@ -24,8 +24,6 @@
 ---@field team Team
 ---@field force LuaForce
 
---TODO: The other events
-
 --- The value used to determine the larger team affiliation of a `Team`
 ---
 --- - `"-"` is never an ally with anyone
@@ -38,6 +36,9 @@
 
 --- The internal id for a given team, also corresponds to the name of a LuaForce.
 ---@alias TeamID string
+
+--- A number that is multiplied by `60*60` before being used as a `MapTick`
+---@alias Minutes double
 
 ---@class SelectedOptions<V>
 ---@field options V[]
@@ -85,42 +86,69 @@
 --- Selectes what in `inventory_list` can and is being used
 ---@field starting_chest SelectedOptions<string>
 
---TODO: Check values in individual configs
 
 ---@class GameConfig
----@field time_limit MapTick
----@field allow_spectators boolean
----@field no_rush_time MapTick
----@field base_exclusion_time MapTick
+--- How long a game can run.
+---@field time_limit Minutes
+---@field allow_spectators boolean Unused
+--- How long players are restricted to their staring area.
+---@field no_rush_time Minutes
+--- How long players are restricted from entering enemy bases.
+---@field base_exclusion_time Minutes
+--- Whether or not the base locations are revealed at the start of the round.
 ---@field reveal_team_positions boolean
+--- Whether or not the center of the map will be charted at the start of the round.
 ---@field reveal_map_center boolean
+--- Whether or not the walls are built from `PrototypeConfig.wall` and `PrototypeConfig.gate`
 ---@field team_walls boolean
+--- Whether or not a boundry of tiles from `PrototypeConfig.moat` is placed to restrict avenues of approach
 ---@field team_moat boolean
----@field team_turrets boolean
----@field turret_ammunition SelectedOptions<data.ItemName>
+--- Whether or not `PrototypeConfig.artillery` is placed with a stack of 20 `PrototypeConfig.artillery_ammo`.
 ---@field team_artillery boolean
----@field auto_new_round_time MapTick
+--- Whether or not `PrototypeConfig.turret` are placed with a stack of 20 `GameConfig.turret_ammunition`.
+---@field team_turrets boolean
+--- What ammunition the initial turrets will receive a stack of 20 of
+---@field turret_ammunition SelectedOptions<data.ItemName>
+--- How quickly a new round will be autostarted. `0` means it will not autostart a round.
+---@field auto_new_round_time Minutes
+--- Whether or not all entities on a force should be made non-destructable when there are no connected players.
 ---@field protect_empty_teams boolean
+--- Whether or not players can build within enemy bases.
 ---@field enemy_building_restriction boolean
+--- Whether or not `"container"` type entities are converted into a neutral force when built. This lets players steal.
 ---@field neutral_chests boolean
 ---@field seed uint32
 
 
 ---@class TeamConfig
 ---@field friendly_fire boolean
----@field unlock_combat_research boolean
+---@field unlock_combat_research boolean Unused
+--- A mode where technology is researched randomly and automatically. Players cannot affect research.
 ---@field defcon_mode boolean
 ---@field max_players int
----@field defcon_timer MapTick
----@field starting_chest_multiplier int
+--- How quickly technology should be researched.
+---@field defcon_timer Minutes
+--- Multiplies the item count for whatever items are being given in `Config.inventory_list`
+---@field starting_chest_multiplier double
+--- The selected value determines at what point while going through the options that the ingredients are no longer going to be used to research initial technologies.
+---
+--- Eg: a selected `"logistic-science-pack"` with options `{"none", "automation-science-pack", "logistic-science-pack", ..etc}`
+--- will research (almost) all technologies that have only automation, or logistic science packs.
+---
+--- `Config.research_ingredient_list` can filter out ingredients from within this list by setting them to `nil`,
+--- as well as `TeamConfig.unlock_combat_research` determining whether "combat" research is unlocked.
 ---@field research_level SelectedOptions<"none"|data.ItemName>
----@field average_team_displacement number
+--- If found to be smaller than the minimum distance as determined by the starting radius, then it will be automatically increased.
+---@field average_team_displacement double
 ---@field always_day boolean
+--- Clamped to `[0,1]`
 ---@field evolution_factor double
+--- Whether or not the entities within the starting area are identical for each team
 ---@field duplicate_starting_area_entities boolean
----@field technology_price_multiplier double
+--- Default: `1`
+---@field technology_price_multiplier? double
 
---NOTE: Some of these probably don't have to be specific entity types.
+--NOTE: Some of these probably don't have to be specific entity types. These are just the ones they are intended for
 ---@class PrototypeConfig
 ---@field chest data.ContainerName
 ---@field wall data.WallName
@@ -131,7 +159,7 @@
 ---@field silo data.RocketSiloName
 ---@field tile_1 data.TileName
 ---@field tile_2 data.TileName
----@field artillery_remote data.SelectionToolName
+---@field artillery_remote data.SelectionToolName Unused
 ---@field oil data.FluidName
 ---@field oil_resource data.FluidName Unused
 ---@field moat data.TileName
@@ -146,13 +174,13 @@
 ---@class VictoryCondition.base
 ---@field active boolean
 ---@class VictoryCondition.space_race : VictoryCondition.base
----@field required_rockets_sent number
+---@field required_rockets_sent double
 ---@class VictoryCondition.production_score : VictoryCondition.base
----@field required_production_score number
+---@field required_production_score double
 ---@class VictoryCondition.oil_harvest : VictoryCondition.base
----@field required_oil number
+---@field required_oil double
 ---@class VictoryCondition.kill_score : VictoryCondition.base
----@field required_kill_score number
+---@field required_kill_score double
 
 
 ---@class RemoteInterface
